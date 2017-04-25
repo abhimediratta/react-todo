e77a3fd204811f292ea4371af8dada0910b0d294
abhimediratta <abhimediratta@gmail.com>
2017-04-25 16:57:10 +0530

    Spread operator

---
+++ playground/spread.js
@@ -0,0 +1,23 @@
+// function add(a, b) {
+//   return a + b;
+// }
+//
+// console.log(add(3, 1));
+//
+// var toAdd = [9, 5];
+// console.log(add(...toAdd));
+
+
+// var groupA = ['Aniket', 'Pranil'];
+// var groupB = ['Chetan'];
+//
+// var final = [...groupB, 3, ...groupA];
+// console.log(final);
+
+
+var personA = ['Abhi', 25];
+var personB = ['Aniket', 30];
+// Hi Abhi, you're 25
+
+var names = ['Mike', 'Ben'];
+var final = ['Abhi'];
