bb2f3492a4629aff51028e1b7b8b156d05669f24
abhimediratta <abhimediratta@gmail.com>
2017-06-07 17:36:47 +0530

    Store Todos in local Storage

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,7 @@ var {Provider} = require('react-redux');
 
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var TodoApp = require('TodoApp');
+var TodoAPI = require('TodoAPI');
 
 // Load foundation
 $(document).foundation();
@@ -15,12 +16,15 @@ var actions = require('actions');
 var store = require('configureStore').configure();
 
 store.subscribe(() => {
-  console.log('New state', store.getState());
+  var state = store.getState();
+  console.log('New state', state);
+
+  TodoAPI.setTodos(state.todos);
 });
 
-store.dispatch(actions.addTodo('Clean the yard'));
-store.dispatch(actions.setSearchText('yard'));
-store.dispatch(actions.toggleShowCompleted());
+var initialTodos = TodoAPI.getTodos();
+store.dispatch(actions.addTodos(initialTodos));
+
 
 ReactDOM.render(
   <Provider store={store}>
